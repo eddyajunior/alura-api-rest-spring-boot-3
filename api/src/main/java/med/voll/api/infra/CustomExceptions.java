ead819977a333dd87f5a6ab4547d8a836f3df470
Edson Amaral <eddyajunior@gmail.com>
2024-03-22 18:17:29 -0300

    feat: Adiciona o tratamento para o erro 400

--- api/src/main/java/med/voll/api/infra/CustomExceptions.java
+++ api/src/main/java/med/voll/api/infra/CustomExceptions.java
@@ -2,6 +2,7 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,10 @@ public class CustomExceptions
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity CustomError404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity CustomError400(){
+        return ResponseEntity.badRequest().build();
     }
 }
